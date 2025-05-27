09eead3b20d561091cdae39fe2507cd16f4aa6a6
sakurasen <2402147211@qq.com>
2025-05-28 01:37:29 +0800

    On branch main

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import axios from 'axios'
 import js from './js.vue'
 const app = createApp(APP)
 const JS = createApp(js)
-app.use(eventBus)
 app.config.globalProperties.$axios = axios
 
 JS.mount('#st')
