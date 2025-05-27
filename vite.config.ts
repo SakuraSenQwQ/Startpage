7013df006a16b678812f06756c792b44b5a3e252
sakurasen <2402147211@qq.com>
2025-05-28 05:15:02 +0800

    On branch main

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/startpage',
+  base: '/startpage/',
   server: {
     host: '0.0.0.0',
   },
